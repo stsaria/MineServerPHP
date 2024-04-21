635352ccdd5608bb97657cf048779a075d066a48
stsaria <solothunder.autoer@gmail.com>
2024-04-21 12:30:40 +0900

    asd

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 mkdir minecraft log data minecraft/log
 cd data && touch mcversions.json server.csv && echo 0 > createNum.txt && cd ..
 cd log && touch php.log python.log && cd ..
-sudo apt install python3-pip php screen -y
+sudo apt install python3-full php screen -y
 pip3 install requests
